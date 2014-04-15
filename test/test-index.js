064cdde4d443c8ecfc69f327a35be69eb82e5b45
Michelle Becker <michelle.becker@caa.com>
2014-04-15 12:09:37 -0700

    list asset tests

--- test/test-index.js
+++ test/test-index.js
@@ -3,6 +3,7 @@ var AMSService = require('../')
 var config     = require('../test-config')
 
 var amsService
+var assestId
 
 describe('AMS Service', function () {
 
@@ -34,6 +35,50 @@ describe('AMS Service', function () {
       expect(amsService).to.have.property('request')
 
     })
+
+    it('should set the access token', function (done){
+
+      amsService.setToken(function (err) {
+
+        expect(err).to.not.exist
+        expect(amsService.request.token).to.exist
+        expect(amsService.request.tokenExpires).to.exist
+
+        done()
+
+      })
+    })
+
+    it('should list assets - cb ', function (done) {
+
+      amsService.listAssets(function (err, res) {
+
+        expect(err).to.not.exist
+        expect(res).to.exist
+        expect(res.body).to.exist
+
+        done()
+      })
+    })
+
+    it('should list assets - stream ', function (done) {
+
+      var data = ''
+
+      amsService.listAssets()
+      .on('data', function(d){
+        data += d
+      })
+      .on('error', function(e){
+        expect(e).to.not.exist
+        done()
+      })
+      .on('end', function(){
+        expect(data).to.exist
+        done()
+      })
+    })
+
 
 
   })
